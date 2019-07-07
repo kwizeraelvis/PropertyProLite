ac2e9d6767c149eda464e488dde05447338fa536
amily kassim <amilykassim02@gmail.com>
2019-07-07 23:15:05 +0200

    [feature #167119752] validate if user is already registered

--- server/src/controllers/signup.js
+++ server/src/controllers/signup.js
@@ -7,6 +7,9 @@ const signup = async (req, res) => {
   const { error } = validate(req);
   if (error) return res.status(400).send(results(ERROR, error.details[0].message));
 
+  let user = users.find(user => user.email === req.body.email);
+  if (user) return res.status(400).send(results(ERROR, 'user already registered.'));
+
 
 };
 
