343c41135ee09bf3436a2dd313874b18c4cf47b4
amily kassim <amilykassim02@gmail.com>
2019-07-07 23:41:50 +0200

    [feature #167120331]validate user password

--- server/src/controllers/signin.js
+++ server/src/controllers/signin.js
@@ -4,8 +4,26 @@ import { users, generateAuthToken } from '../models/user';
 import { results, SUCCESS, ERROR } from '../helper/result';
 
 
+function validate(req) {
+  const schema = {
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  };
+
+  return Joi.validate(req.body, schema);
+}
+
 const signin = async (req, res) => {
-  res.send('it is working')
+  const { error } = validate(req);
+  if (error) return res.status(400).send(results(ERROR, error.details[0].message));
+
+  const user = users.find(user => user.email === req.body.email);
+  if (!user) return res.status(400).send(results(ERROR, 'Invalid email'));
+
+  const isValid = await bcrypt.compare(req.body.password, user.password);
+  if (!isValid) return res.status(400).send(results(ERROR, 'Invalid password'));
+
+ 
 };
 
 export default signin;
