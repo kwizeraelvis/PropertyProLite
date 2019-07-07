ba981ff6438fb933e16c911d227a74b3fd30f04a
amily kassim <amilykassim02@gmail.com>
2019-07-07 23:17:24 +0200

    [feature #167119752] hash user password

--- server/src/controllers/signup.js
+++ server/src/controllers/signup.js
@@ -16,6 +16,9 @@ const signup = async (req, res) => {
   const token = generateAuthToken(user);
   user.token = token;
 
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
 
 };
 
