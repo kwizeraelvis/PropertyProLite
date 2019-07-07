d653fafd92c445853a67c44bb8d6e7b3dbbc8771
amily kassim <amilykassim02@gmail.com>
2019-07-07 23:13:31 +0200

    [feature #167119752] validate if user inputs is valid

--- server/src/controllers/signup.js
+++ server/src/controllers/signup.js
@@ -4,7 +4,10 @@ import { users, validate, generateAuthToken } from '../models/user';
 import { results, SUCCESS, ERROR } from '../helper/result';
 
 const signup = async (req, res) => {
-  res.send('it is working');
+  const { error } = validate(req);
+  if (error) return res.status(400).send(results(ERROR, error.details[0].message));
+
+
 };
 
 export default signup;
