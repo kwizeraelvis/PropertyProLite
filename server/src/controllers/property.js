ca77bd4bb82694cc0a083bd3324c492e78477a64
amily kassim <amilykassim02@gmail.com>
2019-07-07 23:53:06 +0200

    [feature #167120394] save property data

--- server/src/controllers/property.js
+++ server/src/controllers/property.js
@@ -7,5 +7,17 @@ import cloudinary from '../startup/cloudinary';
 
 
 export const postProperty = async (req, res) => {
-  res.send('it is really working brozzay');
+  
+
+  const { error } = validate(req);
+  if (error) return res.status(400).send(results(ERROR, error.details[0].message));
+
+  const property = _.pick(req.body, ['price', 'state', 'city', 'address', 'type', 'image_url']);
+  property.id = properties.length + 1;
+  property.owner = req.user.id;
+  property.status = 'available';
+  property.created_on = new Date().toLocaleString();
+
+  properties.push(property);
+
 };
