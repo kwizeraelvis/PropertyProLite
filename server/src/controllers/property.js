2236a06ec142b79deb081a28ebf00d9ed5d19705
amily kassim <amilykassim02@gmail.com>
2019-07-08 00:30:57 +0200

    [feature #167120574] update property

--- server/src/controllers/property.js
+++ server/src/controllers/property.js
@@ -67,5 +67,13 @@ export const postProperty = async (req, res) => {
 };
 
 export const updateProperty = async (req, res) => {
- res.send('update property is working');
+  const property = properties.find(p => p.id === parseInt(req.params.id, 10));
+  if (!property) return res.status(404).send(results(ERROR, 'Property with the given id does not exists'));
+
+  const keys = Object.keys(property);
+  keys.forEach((key) => {
+    if (!['id', 'owner', 'status', 'created_on'].includes(key) && req.body[`${key}`]) 
+        property[`${key}`] = req.body[`${key}`];
+  });
+
 };
