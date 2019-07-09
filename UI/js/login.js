c297eeade7443f9f60f4b7cb779924d4fcfe4e19
amily kassim <amilykassim02@gmail.com>
2019-07-09 05:55:21 +0200

    feature(#FINISHES 167149377) creating a landing page

--- UI/js/login.js
+++ UI/js/login.js
@@ -1,13 +1,19 @@
 const loginBtn = document.querySelector("#login");
-const signupBtn = document.querySelector("#signup");
+const signupBtn = document.querySelector("#signup-navbar");
+const forgotPassword = document.querySelector("#forgot-password");
 
 loginBtn.addEventListener("click", goToHomepage);
 signupBtn.addEventListener("click", goToSignUpPage);
+forgotPassword.addEventListener("click", resetPassword);
 
 function goToHomepage() {
-    window.location.href = "html/home.html";
+    window.location.href = "home.html";
 }
 
 function goToSignUpPage() {
-    window.location.href = "html/signup.html";
+    window.location.href = "signup.html";
+}
+
+function resetPassword() {
+    window.location.href = "reset_password.html";
 }
