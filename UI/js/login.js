64d37e409e7e390d366710aff9965713b8f0fe31
amily kassim <amilykassim02@gmail.com>
2019-07-09 10:56:26 +0200

    chore(landing_page) grouping landing page css styles [FINISHES #167149377]

--- UI/js/login.js
+++ UI/js/login.js
@@ -1,10 +1,13 @@
 const loginBtn = document.querySelector("#login");
 const signupBtn = document.querySelector("#signup-navbar");
 const forgotPassword = document.querySelector("#forgot-password");
+const backBtn = document.querySelector("#back");
 
 loginBtn.addEventListener("click", goToHomepage);
 signupBtn.addEventListener("click", goToSignUpPage);
 forgotPassword.addEventListener("click", resetPassword);
+backBtn.addEventListener("click", goToLandingPage);
+
 
 function goToHomepage() {
     window.location.href = "home.html";
@@ -16,4 +19,8 @@ function goToSignUpPage() {
 
 function resetPassword() {
     window.location.href = "reset_password.html";
+}
+
+function goToLandingPage() {
+    window.location.href = "../index.html";
 }
