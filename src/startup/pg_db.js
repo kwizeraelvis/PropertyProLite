06e1f6d6dfca2fcd63a640d0f6541532bfb54949
amily kassim <amilykassim02@gmail.com>
2019-07-16 15:10:15 +0200

    [FINISHES #167311325] delete env files and hard coded connection string

--- src/startup/pg_db.js
+++ src/startup/pg_db.js
@@ -3,16 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectionString = 'postgresql://postgres:actionking@localhost:5432/property_pro_lite';
 let pool;
 
 if(process.env.NODE_ENV === 'test') {
     pool = new Pool({ connectionString: process.env.DATABASE_TEST });
-    // await pool.query('CREATE DATABASE travis_ci_test');
 }
 
 if(process.env.NODE_ENV === 'development') {
-    pool = new Pool({ connectionString });
+    pool = new Pool({ connectionString: process.env.DATABASE_DEVELOPMENT });
 }
 
 console.log('the running environment is : ', process.env.NODE_ENV);
