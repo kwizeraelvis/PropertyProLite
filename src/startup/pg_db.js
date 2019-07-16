7732b7cf688bad929e1c3bcb0a2813485d547599
amily kassim <amilykassim02@gmail.com>
2019-07-16 12:27:33 +0200

    [finishes #167311325] adding local url to env variables

--- src/startup/pg_db.js
+++ src/startup/pg_db.js
@@ -3,17 +3,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectionString = 'postgressql://postgres:actionking@localhost:5432/property_pro_lite';
+const connectionString = 'postgresql://postgres:actionking@localhost:5432/property_pro_lite';
 let pool;
 
 if(process.env.NODE_ENV === 'test') {
     pool = new Pool({ connectionString: process.env.DATABASE_TEST });
+    // await pool.query('CREATE DATABASE travis_ci_test');
 }
 
 if(process.env.NODE_ENV === 'development') {
     pool = new Pool({ connectionString });
 }
 
+console.log('the running environment is : ', process.env.NODE_ENV);
 
 export { pool };
 
