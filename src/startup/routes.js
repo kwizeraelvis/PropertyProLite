0d6db42a13ccc2ef738903f9a8a33c2c1a4777c7
amily kassim <amilykassim02@gmail.com>
2019-07-18 09:18:36 +0200

    [finishes #167355671] add protection to all endpoints

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -4,6 +4,7 @@ import signup from '../routes/signup';
 import signin from '../routes/signin';
 import property from '../routes/property';
 import error from '../middleware/user/error';
+import { results, ERROR } from '../helper/result';
 
 
 export default (app) => {
@@ -13,5 +14,6 @@ export default (app) => {
   app.use('/api/v1/auth/signup', signup);
   app.use('/api/v1/auth/signin', signin);
   app.use('/api/v1/property', property);
+  app.use('*', (req, res) => { res.status(400).send(results(400, ERROR, { 'message': 'The route is invalid' })) });
   app.use(error);
 };
